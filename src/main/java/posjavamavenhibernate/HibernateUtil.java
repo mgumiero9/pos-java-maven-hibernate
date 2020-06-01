d4557e8d7871d703fc7dc4676e8c7897415cbecc
Marcelo Gumiero <mgumiero9@gmail.com>
2020-06-01 08:34:55 -0300

    Entity Manager implemented

---
+++ src/main/java/posjavamavenhibernate/HibernateUtil.java
@@ -0,0 +1,30 @@
+package posjavamavenhibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    private static EntityManagerFactory factory = null;
+
+    static {
+        init();
+    }
+
+    private static void init() {
+        try {
+            if (factory == null) {
+                factory = Persistence.createEntityManagerFactory("pos-java-maven-hibernate");
+            }
+        } catch (Exception e) {
+            System.out.println("exception: ");
+            e.printStackTrace();
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+}
---
+++ src/test/java/posjavamavenhibernate/HibernateTest.java
@@ -0,0 +1,11 @@
+package posjavamavenhibernate;
+
+import org.junit.Test;
+
+public class HibernateTest {
+
+    @Test
+    public void hibernateTest() {
+        HibernateUtil.getEntityManager();
+    }
+}
